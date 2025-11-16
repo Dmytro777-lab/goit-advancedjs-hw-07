3c75fd91a0bdaa38f4e9062bf5afa699a165dcbe
Dorofeyev Dmytro <dorofeyev74@gmail.com>
2025-11-16 22:13:28 +0100

    Completed task 4: classes Key, Person, House, MyHouse

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person) {
+    if (this.door === true) {
+      return this.tenants.push(person);
+    }
+  }
+}
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      return (this.door = true);
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
